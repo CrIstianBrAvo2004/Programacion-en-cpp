6f660f31c475c335f0cefde3c8922d6b1b76a516
CrIstianBrAvo2004 <cbravon@unal.edu.co>
2024-05-29 21:55:50 -0600

    Up task about organize three numbers

---
+++ build/CMakeFiles/orden_n.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/orden_n.dir/scr/main.cpp.o"
+  "CMakeFiles/orden_n.dir/scr/main.cpp.o.d"
+  "orden_n"
+  "orden_n.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/orden_n.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
